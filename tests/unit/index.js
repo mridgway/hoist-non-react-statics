62c18e9a1570808a261b049cf0864e7de31295b2
Hajime TAKAHASHI <cc.lnn@icloud.com>
2015-12-24 11:54:48 +0900

    Add function for decorator

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -4,6 +4,7 @@
 var expect = require('chai').expect;
 var React = require('react');
 var hoistNonReactStatics = require('../../index');
+var hoistNonReactStaticsFrom = require('../../decorator');
 
 describe('hoist-non-react-statics', function () {
 
@@ -26,6 +27,34 @@ describe('hoist-non-react-statics', function () {
         });
 
         hoistNonReactStatics(Wrapper, Component);
+
+        expect(Wrapper.displayName).to.equal('Bar');
+        expect(Wrapper.foo).to.equal('bar');
+    });
+
+});
+
+describe('hoist-non-react-statics-from', function () {
+
+    it('should hoist non react statics', function () {
+        var Component = React.createClass({
+            displayName: 'Foo',
+            statics: {
+                foo: 'bar'
+            },
+            render: function () {
+                return null;
+            }
+        });
+
+        var Wrapper = React.createClass({
+            displayName: 'Bar',
+            render: function () {
+                return <Component />;
+            }
+        });
+
+        hoistNonReactStaticsFrom(Component)(Wrapper);
 
         expect(Wrapper.displayName).to.equal('Bar');
         expect(Wrapper.foo).to.equal('bar');
