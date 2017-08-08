849de2167671a3e73fbee65a363e1b695aa6bb45
theduke <reg@theduke.at>
2017-08-08 14:49:53 +0200

    Fix TypeScript definitions
    This fixes 2 issues with the TypeScript definitions:
    
    * Makes it work even if the allowSyntehticDefaultImports is disabled (also fixed by https://github.com/mridgway/hoist-non-react-statics/pull/36)
    
    * Fixes definitions for 2.4, which does not allow `export = ...`

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,7 @@
 declare module 'hoist-non-react-statics' {
-  import React from 'react';
-  function hoistNonReactStatics<Own, Custom>(
+  import * as React from 'react';
+  export default function hoistNonReactStatics<Own, Custom>(
     TargetComponent: React.ComponentType<Own>,
     SourceComponent: React.ComponentType<Own & Custom>,
     customStatic?: any): React.ComponentType<Own>;
-
-  export = hoistNonReactStatics;
 }
