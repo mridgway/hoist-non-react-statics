444f3e54eb473291639fd95c36eb8dd9de7a889d
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-27 12:13:21 +0100

    Do not mix CJS require with ESM format

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
-import uglify from 'rollup-plugin-uglify'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
@@ -8,11 +11,29 @@ const camelCase = string => {
     return first + rest.map(str => str[0].toUpperCase() + str.slice(1)).join('')
 };
 
+const makeExternalPredicate = externalArr => {
+    if (externalArr.length === 0) {
+        return () => false;
+    }
+    const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+    return id => pattern.test(id);
+};
+
 const configBase = {
     input: 'src/index.js',
     plugins: [
-        babel({ exclude: 'node_modules/** '})
-    ]
+        babel({ exclude: 'node_modules/** '}),
+        nodeResolve(),
+        commonjs({
+            namedExports: {
+                'react-is': Object.keys(require('react-is')),
+            },
+        }),
+    ],
+    external: makeExternalPredicate([
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+    ]),
 };
 
 const umdConfig = mergeAll([
@@ -23,7 +44,11 @@ const umdConfig = mergeAll([
             format: 'umd',
             name: camelCase(pkg.name),
         },
-    }
+        plugins: configBase.plugins.concat(
+            replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
+        ),
+        external: makeExternalPredicate(Object.keys(pkg.peerDependencies || {}))
+    },
 ]);
 
 
@@ -31,8 +56,8 @@ const prodUmdConfig = mergeAll([
     umdConfig,
     { output: mergeAll([umdConfig.output, { file: umdConfig.output.file.replace(/\.js$/, '.min.js') }]) },
     {
-        plugins: [
-            babel({ exclude: 'node_modules/** '}),
+        plugins: configBase.plugins.concat(
+            replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
             uglify({
                 compress: {
                     pure_getters: true,
@@ -41,7 +66,7 @@ const prodUmdConfig = mergeAll([
                     warnings: false,
                 },
             })
-        ],
+        ),
     },
 ]);
 
