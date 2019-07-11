8ab0cd331177a8835e0bb1c5ff54127fcbc3430e
Bogdan Chadkin <trysound@yandex.ru>
2019-07-11 19:57:57 +0300

    Simplify rollup config (#84)
    - removed a bunch of merges from config for readability
    - replaced uglify with more trusty terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,42 @@
-import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
-const mergeAll = objs => Object.assign({}, ...objs);
 const camelCase = string => {
-    const [first, ...rest] = string.split('-').map(str => str.toLowerCase());
-    return first + rest.map(str => str[0].toUpperCase() + str.slice(1)).join('')
+  const [first, ...rest] = string.split('-').map(str => str.toLowerCase());
+  return first + rest.map(str => str[0].toUpperCase() + str.slice(1)).join('')
 };
 
-const configBase = {
-    input: 'src/index.js',
+const input = 'src/index.js';
+const name = camelCase(pkg.name);
+
+export default [
+  {
+    input,
+    output: { file: pkg.main, format: 'cjs' },
     plugins: [
-        babel({ exclude: 'node_modules/** '})
+      babel({ exclude: /node_modules/ }),
     ]
-};
-
-const umdConfig = mergeAll([
-    configBase,
-    {
-        output: {
-            file: `dist/${pkg.name}.js`,
-            format: 'umd',
-            name: camelCase(pkg.name),
+  },
+  {
+    input,
+    output: { file: `dist/${pkg.name}.js`, format: 'umd', name },
+    plugins: [
+      babel({ exclude: /node_modules/ })
+    ]
+  },
+  {
+    input,
+    output: { file: `dist/${pkg.name}.min.js`, format: 'umd', name },
+    plugins: [
+      babel({ exclude: /node_modules/ }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
         },
-    }
-]);
-
-
-const prodUmdConfig = mergeAll([
-    umdConfig,
-    { output: mergeAll([umdConfig.output, { file: umdConfig.output.file.replace(/\.js$/, '.min.js') }]) },
-    {
-        plugins: [
-            babel({ exclude: 'node_modules/** '}),
-            uglify({
-                compress: {
-                    pure_getters: true,
-                    unsafe: true,
-                    unsafe_comps: true,
-                },
-            })
-        ],
-    },
-]);
-
-const cjsConfig = mergeAll([
-    configBase,
-    { output: { file: pkg.main, format: 'cjs' } },
-]);
-
-export default [umdConfig, prodUmdConfig, cjsConfig]
+      })
+    ]
+  }
+]
