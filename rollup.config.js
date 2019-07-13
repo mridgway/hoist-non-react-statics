edb25c33cda3f990672a5e23e17828aa392c7d3c
Bogdan Chadkin <trysound@yandex.ru>
2019-07-13 21:57:28 +0300

    Fix umd bundle (#85)
    * Fix umd bundle
    
    Currently umd calls not defined `require` function. In this diff I
    bundled `react-is` in umd to let it work in browsers.
    
    Nothing is changed for cjs build.
    
    Closes https://github.com/mridgway/hoist-non-react-statics/pull/77 and https://github.com/mridgway/hoist-non-react-statics/pull/58
    
    * Infer named exports from runtime

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
+import path from 'path';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
+import * as ReactIs from 'react-is';
 
 const camelCase = string => {
   const [first, ...rest] = string.split('-').map(str => str.toLowerCase());
@@ -9,11 +13,18 @@ const camelCase = string => {
 
 const input = 'src/index.js';
 const name = camelCase(pkg.name);
+const external = id => !id.startsWith('.') && !path.isAbsolute(id);
+const commonjsOptions = {
+  namedExports: {
+    'react-is': Object.keys(ReactIs)
+  }
+}
 
 export default [
   {
     input,
     output: { file: pkg.main, format: 'cjs' },
+    external,
     plugins: [
       babel({ exclude: /node_modules/ }),
     ]
@@ -22,14 +33,18 @@ export default [
     input,
     output: { file: `dist/${pkg.name}.js`, format: 'umd', name },
     plugins: [
-      babel({ exclude: /node_modules/ })
+      nodeResolve(),
+      babel({ exclude: /node_modules/ }),
+      commonjs(commonjsOptions)
     ]
   },
   {
     input,
     output: { file: `dist/${pkg.name}.min.js`, format: 'umd', name },
     plugins: [
+      nodeResolve(),
       babel({ exclude: /node_modules/ }),
+      commonjs(commonjsOptions),
       terser({
         compress: {
           pure_getters: true,
